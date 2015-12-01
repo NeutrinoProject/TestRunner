762e1655776fc3bfb98282ba9a10d40b854d097a
btv2018 <otstoy.reg@gmail.com>
2015-12-01 20:34:48 +0100

    add mockito test library

--- test/com/neutrinoproject/testrunner/TestOutputParserTest.java
+++ test/com/neutrinoproject/testrunner/TestOutputParserTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import static org.mockito.Mockito.*;
+
 /**
  * Created by btv on 30.11.15.
  */
@@ -11,6 +13,31 @@ public class TestOutputParserTest {
 
     @Test
     public void testParseString() throws Exception {
-        assertEquals("0 must be 0", 0, 0);
+        final TestEventHandler handler = mock(TestEventHandler.class);
+        final TestOutputParser parser = new TestOutputParser(handler);
+
+        final String someText = "some text";
+        parser.parseString(someText);
+        verify(handler).onOutLine(someText);
+
+        parser.parseString(someText);
+        verify(handler, times(2)).onOutLine(someText);
+
+        parser.parseString("[ RUN      ] Neutrino.HasMass");
+        verify(handler).onTestState(TestRunState.STARTED, "Neutrino", "HasMass");
+
+        // Starting a new test before finishing the previous one has no effect.
+        parser.parseString("[ RUN      ] Neutrino.HasMass");
+        verify(handler, times(1)).onTestState(TestRunState.STARTED, "Neutrino", "HasMass");
+
+        parser.parseString("[       OK ] Neutrino.HasMass (0 ms)");
+        verify(handler).onTestState(TestRunState.OK, "Neutrino", "HasMass");
+
+        parser.parseString("[ RUN      ] Neutrino.IsStable");
+        verify(handler).onTestState(TestRunState.STARTED, "Neutrino", "IsStable");
+
+        parser.parseString("[  FAILED  ] Neutrino.IsStable (0 ms)");
+        verify(handler).onTestState(TestRunState.FAILED, "Neutrino", "IsStable");
+
     }
 }
